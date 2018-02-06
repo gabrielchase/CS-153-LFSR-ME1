40af449c4ea3dbc7477476a5fba8dac3db726bef
gabrielchase <gchase.patron@gmail.com>
2018-02-06 22:40:30 +0800

    Add `.gitignore`

--- ME1.py
+++ ME1.py
@@ -3,7 +3,7 @@ def get_binary(val):
 
 def xor(a, b):
     return bool(a) != bool(b)
-
+    
 assert not xor(0, 0)
 assert not xor(1, 1)
 assert xor(0, 1)
@@ -11,13 +11,15 @@ assert xor(1, 0)
 
 
 if __name__ == '__main__':
+    clock = 0
+    
     test_cases = int(input('Enter number of test cases: '))
-    count = 0
     
     while(count < test_cases):
         m_input, s_input, p_input = input().split()
+        
         m = get_binary(m_input)
         s = get_binary(s_input)
         p = get_binary(p_input)
 
-        count += 1 
+        clock += 1 
